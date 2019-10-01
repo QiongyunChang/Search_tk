a977c52c7e63c2009383c3b7c0a931656929d95e
Todoorno <51444652+Todoorno@users.noreply.github.com>
2019-10-01 19:51:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ def function_1():
         win2 = tk.Tk()
         win2.geometry("450x300") #the size of window
         win2.title("Result")
-        txt = tk.Text(win2,height=20) 
+        txt = tk.Text(win2,height=5) 
         txt.pack()
         try : 
-            #connect to wiki
-            wiki=wikipedia.summary(entry, sentences = 3)
-            txt.insert("end",wiki)
-            
-            
-        except:
+            #connect to wolframalpha             
             id = "3QJ59U-539QW66HE8"
             client = wolframalpha.Client(id)
-            res = client.query(entry)
+            res = client.query(estr.get())
             answer = next(res.results).text
             txt.insert("end",answer)
             # print (answer)
-           
+            
+            
+        except:
+            #connect to wiki
+            wiki=wikipedia.summary(estr.get(), sentences = 3)
+            txt.insert('2.0',wiki)
             
         win2.mainloop()
           
@@ -41,7 +41,9 @@ win.title("Search")
 #info for entry 
 #word color = white ,fg='white',bg='SlateGray'
 label = tk.Label(win,text = "Searching ").pack()
-entry = tk.Entry(win).pack()
+estr = tk.StringVar()
+entry = tk.Entry(win,text= estr).pack()
+
 b = tk.Button(win,text="Submit",padx=1,pady=1,command= function_1).pack()
 #interact with users
 win.mainloop() 
